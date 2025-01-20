ccc09e78e1a88bbe3a17ae8e16789b62e1e2ee04
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-01-20 05:37:44 +0200

    rtwo: build health service

--- device.mk
+++ device.mk
@@ -41,3 +41,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock 
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
