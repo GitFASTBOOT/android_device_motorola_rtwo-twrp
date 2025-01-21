b0f2f53b152e5290e2683bafa38e02445b2d1e0d
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2025-01-21 21:14:54 +0200

    rtwo: build full fastboot packages

--- device.mk
+++ device.mk
@@ -40,7 +40,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock 
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock.so \
+    fastbootd
 
 # Health Hal
 PRODUCT_PACKAGES += \
